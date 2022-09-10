da6a23be190d352eaf0cefbc389c598a7551d27d
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-09-10 00:13:37 -0400

    adding questions to index.js

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,55 @@
 // TODO: Include packages needed for this application
+const fs = require ('fs');
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'ProjectName',
+        message: 'What is your project name?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project?'
+    },
+    {
+        type:'input',
+        name: 'installguide',
+        message:'Install guide for your project?'
+        },
+        {
+            type:'input',
+            name: 'usage',
+            message: 'Enter your project usage information.'
+        },
+        {
+            type:'input',
+            name:'contributions',
+            message:'please enter your projets contribution guidelines.',
+        },
+        {
+            type:'input',
+            name:'Instructions',
+            message:'enter the instructions for your project.',
+
+        },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    FileSystem.writeToFile(fileName, data, err? console.log (err) : console.log ('Readme Generated!'));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(anwsers =>{
+        console.log(anwsers);
+    });
+}
 
 // Function call to initialize app
 init();
